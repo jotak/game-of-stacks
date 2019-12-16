7b934d119e09a148cd17245b57eef70082918249
Andy Damevin <ia3andy@gmail.com>
2019-12-16 18:03:50 +0100

    Reset to latest messages for kafka

--- common/src/main/kotlin/demo/gos/common/Commons.kt
+++ common/src/main/kotlin/demo/gos/common/Commons.kt
@@ -10,7 +10,7 @@ import java.util.*
 object Commons {
   private val metricsEnabled = getIntEnv("METRICS_ENABLED", 0)
   @JvmStatic val groupId = UUID.randomUUID().toString()
-  @JvmStatic val kafkaAddress = getStringEnv("KAFKA_ADDRESS", "localhost:9093")
+  @JvmStatic val kafkaAddress = getStringEnv("KAFKA_ADDRESS", "localhost:9092")
   @JvmStatic val kafkaConfigProducer: Map<String, String> = mapOf(
     "bootstrap.servers" to kafkaAddress,
     "key.serializer" to "org.apache.kafka.common.serialization.StringSerializer",
@@ -23,7 +23,7 @@ object Commons {
     "group.id" to groupId,
     "key.deserializer" to "org.apache.kafka.common.serialization.StringDeserializer",
     "value.deserializer" to "io.vertx.kafka.client.serialization.JsonObjectDeserializer",
-    "auto.offset.reset" to "earliest",
+    "auto.offset.reset" to "latest",
     "enable.auto.commit" to "false"
   )
 
