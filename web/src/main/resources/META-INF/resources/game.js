040949204072d7e855963d5fa9883c44b21e34ac
Andy Damevin <ia3andy@gmail.com>
2020-01-28 15:48:02 +0100

    Add explosion for boulder :-)

--- web/src/main/resources/META-INF/resources/game.js
+++ web/src/main/resources/META-INF/resources/game.js
@@ -13,20 +13,38 @@ $("body").append(app.view);
 let elements = {};
 
 let players;
+const explosion = [];
 PIXI.loader
-    .add('assets/players.json')
+    .add('assets/images/players.json')
+    .add('assets/images/explosion.json')
     .load(() => {
-        players = PIXI.loader.resources["assets/players.json"].textures;
-        //const sprite = new PIXI.Sprite(id["varys"]);
-        //sprite.anchor.x = 1
-        //sprite.scale.x = -2;
-        //sprite.scale.y = 2;
-        //Add the rocket to the stage
-        //app.stage.addChild(sprite);
+        players = PIXI.loader.resources["assets/images/players.json"].textures;
 
-        //Render the stage   
+        // Explosion
+        for (let i = 1; i <= 26; i++) {
+            explosion.push(PIXI.Texture.fromFrame(`explosion${i}.png`));
+        }
+
         app.renderer.render(app.stage);
     });
+app.ticker.add(function (delta) {
+    PIXI.tweenManager.update();
+});
+
+function explode(x, y) {
+    const mc = new PIXI.extras.MovieClip(explosion);
+    mc.position.x = x;
+    mc.position.y = y;
+    mc.anchor.x = 0.5;
+    mc.anchor.y = 0.5;
+    mc.loop = false;
+    mc.animationSpeed = 0.6;
+    mc.rotation = Math.random() * Math.PI;
+    mc.scale.set(0.75 + Math.random() * 0.5);
+    mc.onComplete = function() { app.stage.removeChild(mc); };
+    mc.play();
+    app.stage.addChild(mc);
+}
 
 function resetGame() {
     elements = {};
@@ -34,7 +52,7 @@ function resetGame() {
 
 function removeGameObject(obj) {
     if (elements[obj.id]) {
-        app.stage.removeChild(el.sprite);
+        app.stage.removeChild(elements[obj.id].sprite);
         elements[obj.id] = null;
     }
 }
@@ -55,8 +73,23 @@ function displayGameObject(obj) {
             app.stage.addChild(el.sprite);
         } else {
             //console.log(`Updating ${obj.id}`)
-            el.sprite.x = obj.x;
-            el.sprite.y = obj.y;
+            var tween = PIXI.tweenManager.createTween(el.sprite);
+            tween.time = 1000;
+            tween.easing = PIXI.tween.Easing.linear();
+            tween.from({
+                x: el.sprite.x,
+                y: el.sprite.y
+            });
+            tween.to({
+                x: obj.x,
+                y: obj.y
+            });
+            tween.start();
+        }
+        if (obj.action === "explode") {
+            console.log("explode!!!!!!!!!!!!!!!!!!!!!!")
+            removeGameObject(obj);
+            explode(obj.x, obj.y);
         }
     } else {
         const sprite = new PIXI.Sprite(players[obj.sprite + ".png"]);
